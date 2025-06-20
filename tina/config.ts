8ef133b0bed1f819455fd15cb0d8662250bcb0b9
McDene <denervaud.maxime00@gmail.com>
2025-06-20 18:03:26 +0200

    Update Only Docusorus

--- tina/config.ts
+++ tina/config.ts
@@ -9,8 +9,6 @@ const branch =
 
 export default defineConfig({
   branch,
-
-  // Get this from tina.io
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   // Get this from tina.io
   token: process.env.TINA_TOKEN,
